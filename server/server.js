a2b0dbdf6b9677f096e3c381ed9e916575fc04c1
Sean Oyler <xrysen@gmail.com>
2021-04-12 16:49:18 -0700

    Sets up receiving data from client form

--- server/server.js
+++ server/server.js
@@ -2,10 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const PORT = process.env.PORT || 8080;
+const data = require("./data/data");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+app.get("/data", (req, res) => {
+  res.send(data.data);
+});
+
+// Adds data from form to the "database"
+
+app.post("/data", (req, res) => {
+  data.data.push(req.body);
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
