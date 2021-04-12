b26c6ea58e9a84985baa82ac882fe8d39bff3193
Sean Oyler <xrysen@gmail.com>
2021-04-12 16:45:49 -0700

    Adds seed data

---
+++ server/data/data.js
@@ -0,0 +1,24 @@
+/* sets up some seed data as well as an array of objects to hold information sent from front end */
+
+const data = [
+  {
+    id: 1,
+    city: "Chemainus",
+    country: "Canada",
+    time: "11:35",
+    duckQty: 6,
+    food: "Quakers",
+    foodQty: 100
+  },
+  {
+    id: 2,
+    city: "Victoria",
+    country: "Canada",
+    time: "18:50",
+    duckQty: 8,
+    food: "Bread",
+    foodQty: 150
+  }
+]
+
+exports.data = data;
