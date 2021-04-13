165ac9a67870bdcd25c482124a12cb87f33be09a
Sean Oyler <xrysen@gmail.com>
2021-04-13 11:02:49 -0700

    Adds fade transitions and some styling updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useState } from "react";
+import { AnimateOnChange } from "react-animation";
 import Form from "./Components/Form";
 import Intro from "./Components/Intro";
 import Success from "./Components/Success";
@@ -13,11 +14,22 @@ const RESULTS = "RESULTS";
 function App() {
   const [mode, setMode] = useState(INTRO);
   return (
-    <div className="App">
-      {mode === INTRO && <Intro form={() => setMode(FORM)} results = {() => setMode(RESULTS) }/>}
-      {mode === FORM && <Form back = {() => setMode(INTRO)} success = {() => setMode(SUCCESS) } />}
-      {mode === SUCCESS && <Success back = {() => setMode(INTRO)} results = {()=> setMode(RESULTS)} /> }
-      {mode === RESULTS && <Results back = {() => setMode(INTRO)} /> }
+    <div className = "container">
+      <AnimateOnChange duration={1000}>
+        {mode === INTRO && (
+          <Intro form={() => setMode(FORM)} results={() => setMode(RESULTS)} />
+        )}
+        {mode === FORM && (
+          <Form back={() => setMode(INTRO)} success={() => setMode(SUCCESS)} />
+        )}
+        {mode === SUCCESS && (
+          <Success
+            back={() => setMode(INTRO)}
+            results={() => setMode(RESULTS)}
+          />
+        )}
+        {mode === RESULTS && <Results back={() => setMode(INTRO)} />}
+      </AnimateOnChange>
     </div>
   );
 }
